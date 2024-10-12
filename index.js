be226f87f1e322798d663e48f882f54925ba4c54
DESKTOP-54ATNKA\ASIF <asifbuet07@gmail.com>
2024-10-13 02:46:17 +0600

    Setup MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
@@ -8,6 +9,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//MongoDB connection string
+const uri =
+  "mongodb+srv://bootcamp_db_user_asif:fDvTsgtunlEaMS24@cluster0.jrade.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch((error) => {
+  console.log(error);
+});
+
+//Connecting server
 app.get("/", (req, res) => {
   res.send("React CRUD server is running!");
 });
@@ -17,4 +50,4 @@ app.listen(port, () => {
 });
 
 // bootcamp_db_user_asif
-// 12345678
+// fDvTsgtunlEaMS24
